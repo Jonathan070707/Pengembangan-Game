653eebc892a6363b7a5df2e440be129ae9ca8aaf
JeremiaL <jeremylelemboto@gmail.com>
2024-12-10 22:57:24 +0800

    Initial commit

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,10 +1,26 @@
 extends Control
 
+@onready var option_menu = $OptionScene  # Menu opsi
+@onready var allbutton = $MarginContainer
+@onready var music = $BgmSound
+
+func _ready() -> void:
+	option_menu.visible = false
+
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_option_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/option.tscn")
+	option_menu.visible = true  # Menampilkan opsi sebagai popup
+	allbutton.visible = false
+	# Menonaktifkan tombol agar tidak bisa diklik
+
+
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_option_scene_focus_entered() -> void:
+	allbutton.visible = true
--- scripts/game.gd
+++ scripts/game.gd
@@ -3,10 +3,19 @@ extends Node2D
 class_name Game
 
 @onready var player = get_node("/root/Game/Player")
+@onready var boss_timer = $spawn_stronger  # Timer untuk musuh bos
+@onready var boss_parent = $Enemy  # Parent node untuk menaruh musuh
+
+@export var boss_spawn_time: float = 450.0  # Waktu spawn boss dalam detik
+var boss_enemy_scene = preload("res://scenes/enemy_strong.tscn")  # Path ke scene musuh bos
+var boss_instance: Node = null  # Instance musuh bos
+
 
 #Pause
 signal toggle_name_paused(is_paused : bool) #signal for pause
 signal changetime(time)
+
+
 
 var game_paused : bool = false:
 	get:
@@ -15,6 +24,8 @@ var game_paused : bool = false:
 		game_paused = value
 		get_tree().paused = game_paused
 		emit_signal("toggle_name_paused", game_paused)
+
+
 
 #restart
 func reset_game_state() -> void:
@@ -27,6 +38,12 @@ func reset_game_state() -> void:
 	%End.visible = false
 	%GameOver.visible = false
 	%GameOverSfx.stop()
+	
+	if boss_instance and is_instance_valid(boss_instance):
+		boss_instance.queue_free()
+		boss_instance = null
+		boss_timer.stop()
+	
 # Timer for game duration (10 minutes)
 var game_duration_timer = 600
 var time = 0
@@ -46,6 +63,8 @@ func _ready() -> void:
 		push_error("Player tidak ditemukan! Pastikan grup 'player' sudah ada.")
 	# Mulai timer untuk musuh biasa
 	$Timer.start()
+	boss_timer.wait_time = boss_spawn_time  # Atur waktu untuk spawn boss
+	boss_timer.start(boss_spawn_time)
 	set_process(true)  # Mengaktifkan fungsi _process untuk mengurangi waktu permainan
 
 func _process(delta: float) -> void:
@@ -79,3 +98,17 @@ func _on_sound_game_over_timeout() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		game_paused = !game_paused
+
+
+func spawn_boss() -> void:
+	if not boss_instance:
+		boss_instance = boss_enemy_scene.instantiate()
+		boss_instance.global_position = spawn_point.global_position
+		boss_instance.player = player  # Kirim referensi pemain ke musuh
+		boss_parent.add_child(boss_instance)
+		print("Boss spawned!")
+
+
+
+func _on_spawn_stronger_timeout() -> void:
+	spawn_boss()
